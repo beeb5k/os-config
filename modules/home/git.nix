14f6cddd108c07b35f284d4e1d6788fd73e846c9
beeb5k <beeb5k6@gmail.com>
2025-03-11 17:11:11 +0530

    feat: add GPG signing configuration for Git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,6 +4,11 @@
     userName = "beeb5k";
     userEmail = "beeb5k6@gmail.com";
 
+    signing = {
+      key = "1D8222FA8B7E93A6";
+      signByDefault = true;
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
